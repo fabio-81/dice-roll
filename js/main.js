df01a1908a1d942e86ec62448223102445e1f807
Fabio <fabiog@live.com.pt>
2020-04-13 16:46:08 -0400

    Added non tech explanation about solving the issue

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@
 let roll = Math.floor(Math.random() * 6) + 1
 
 console.log(roll);  // 1, 2, 3, 4, 5, or 6
+
+
+//***** Right now you are: *******
+
+// - Building the html and css to look as if we rolled 3
+// - Using the browser's developer tools to modify the document to look as if you rolled a 5
+// - Write out the "micro" steps you took, like this....
+
+//find the .face image
+
+//Change the value of the src attribute of the .face image
+
+//locate the .result output
+
+//Change the value of the content within to reflect the new roll
+
+//look for the .roll button
+
+//Listen for the .roll button to be clicked
